d72b74c2cf063575346bac3357eb05501f540720
lightlana <lebendaufdensternen@gmail.com>
2022-09-26 22:01:19 +0300

    Программа, которая с помощью деления выводит третью цифру заданного числа или сообщает, что третьей цифры нет.

---
+++ Задача 13/Задача 13.cs
@@ -0,0 +1,18 @@
+﻿void Zadacha13()
+    {
+        Console.WriteLine("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        number = Math.Abs(number);
+        if(number >= 100)
+            {
+                string str = number.ToString();
+                Console.WriteLine(str[2]);
+            }
+        else
+            {
+                Console.WriteLine("Третьей цифры нет");
+            }
+
+    }
+
+Zadacha13();
