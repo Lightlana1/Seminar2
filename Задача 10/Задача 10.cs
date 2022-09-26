da0e3cc934af9a2dc22ed4caff6589ce42cafdfe
lightlana <lebendaufdensternen@gmail.com>
2022-09-26 21:30:39 +0300

    Программа, которая на вход трёхзначное число и на выходе показывает вторую цифру этого числа

---
+++ Задача 10/Задача 10.cs
@@ -0,0 +1,18 @@
+﻿void Zadacha10()
+    {
+        Console.WriteLine("Введите трехзначное число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+    
+        if (number < 1000 && number > 99)
+            {
+                  int tens = number / 10 % 10;
+                  Console.WriteLine(""+tens);
+            }
+        else
+            {
+                Console.WriteLine("Чиcло введено неверно");
+            }
+
+    }
+
+Zadacha10();
